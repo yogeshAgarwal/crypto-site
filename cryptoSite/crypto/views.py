f9a91effad7e4ff20d29277212d305ecaa5ca281
yogeshAgarwal <yogeshagarwal1997199@gmail.com>
2018-09-22 20:47:14 +0530

    add functionalities

--- cryptoSite/crypto/views.py
+++ cryptoSite/crypto/views.py
@@ -2,4 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'crypto/home.html' , {})
+    import requests
+    import json
+
+    #Price Api
+    price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,XLM,ADA,USDT,TRX&tsyms=USD")
+    price = json.loads(price_request.content)
+
+    #news Api
+    api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api = json.loads(api_request.content)
+    return render(request,'crypto/home.html' , {'api':api, 'price':price})
+
+
+def prices(request):
+    if request.method == 'POST':
+        import json
+        import requests
+        quote = request.POST['quote']
+        quote = quote.upper()
+        quote_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + quote + "&tsyms=USD")
+        crypto= json.loads(quote_request.content)
+
+
+        return render(request, 'crypto/prices.html', {'quote':quote, 'crypto':crypto})
+    else:
+        return render(request, 'crypto/prices.html',{})
